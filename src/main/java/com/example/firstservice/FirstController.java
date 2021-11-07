d6d82bb8ccf2e2a41093503709c8252d5932dd80
fortunerain <fortunerain@naver.com>
2021-11-07 22:43:54 +0900

    init

---
+++ src/main/java/com/example/firstservice/FirstController.java
@@ -0,0 +1,29 @@
+package com.example.firstservice;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/first-service")
+@Slf4j
+public class FirstController {
+
+  @GetMapping("/welcome")
+  public String welcome() {
+    return "first service.";
+  }
+
+  @GetMapping("/message")
+  public String message(@RequestHeader("first-request") String header) {
+    log.info(header);
+    return "first service.";
+  }
+
+  @GetMapping("/check")
+  public String check() {
+    return "check. first service.";
+  }
+}
